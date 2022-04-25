7e6e93ec739e18e7f03856ed37a7f60ca1b4a5fd
Lauren A <laurenhaileyama@gmail.com>
2022-04-25 17:40:02 -0400

    Updates to about section and home

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,15 @@ if(navClose){
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //When we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('clock', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
