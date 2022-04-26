16d3908144217195c251e04f71e6587cb6d0e0f7
Lauren A <laurenhaileyama@gmail.com>
2022-04-25 23:23:11 -0400

    meep

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,44 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('clock', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i=0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_Active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})      
+
 
 
 /*==================== SERVICES MODAL ====================*/
